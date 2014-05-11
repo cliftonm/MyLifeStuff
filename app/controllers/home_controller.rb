131985a228e8fca505360e4fe12162072869c789
Marc Clifton <marc.clifton@gmail.com>
2014-05-10 20:24:45 -0400

    WIP on rewriting the table generator.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,136 @@
+require 'html_dsl'
+require 'html_generator'
+
+include ApplicationHelper
+include Airity
+include StyleHelper
+
+class DemoData
+  attr_accessor :field1, :field2, :field3, :field4, :field5
+
+  def initialize(f1, f2, f3, f4, f5)
+    @field1 = f1
+    @field2 = f2
+    @field3 = f3
+    @field4 = f4
+    @field5 = f5
+  end
+
+  def [](field_name)
+    self.send(field_name.to_s)
+    # "#{field_name}=" is the setter.
+  end
+end
+
 class HomeController < ApplicationController
   def test
     @page = MyPage.new()
     @page.content = 'Enter note here'
+  end
+
+  def table_demo
+    r1 = DemoData.new('r1f1', 'r1f2', 'r1f3', 'r1f4', 'r1f5')
+    r2 = DemoData.new('r2f1', 'r2f2', 'r2f3', 'r2f4', 'r2f5')
+    r3 = DemoData.new('r3f1', 'r3f2', 'r3f3', 'r3f4', 'r3f5')
+    r4 = DemoData.new('r4f1', 'r4f2', 'r4f3', 'r4f4', 'r4f5')
+    r5 = DemoData.new('r5f1', 'r5f2', 'r5f3', 'r5f4', 'r5f5')
+    data = [r1, r2, r3, r4, r5]
+    @child_data = [r1]
+
+    html_dsl = HtmlDsl.new()
+
+    # A simple table:
+    html_dsl.div() do
+      html_dsl.label('Simple table:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [5])
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # A simple table with no header:
+    html_dsl.div() do
+      html_dsl.label('Simple table with no header:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [5], {no_header: true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # A simple table with a leading checkbox column:
+    html_dsl.div() do
+      html_dsl.label('Simple table with checkboxes:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [5], {show_checkbox_for_row:true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # Table with three columns on the first row, then two columns on the second row, and checkboxes only on the first row
+    html_dsl.div() do
+      html_dsl.label('Two rows, three columns / two columns with checkbox on the first row:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [3, 2], {show_checkbox_for_row:true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # Table with three columns on the first row, then two columns on the second row, and checkboxes only on the first row, subsequent rows are indented
+    html_dsl.div() do
+      html_dsl.label('Two rows, three columns / two columns with checkbox on the first row, auto-indented:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [3, 2], {show_checkbox_for_row:true, auto_indent:true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # Table with four rows per record, first row has two columns, rest have one column, with column label as first column in table.
+    html_dsl.div() do
+      html_dsl.label('Table with four rows per record, first row has two columns, rest have one column, with column label as first column in table:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [2, 1, 1, 1], {header_as_first_column:true, no_header:true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # Table with four rows per record, first row has two columns, rest have one column, with column label as first column in table and checkbox in the first row, auto-indentation.
+    html_dsl.div() do
+      html_dsl.label('Table with four rows per record, first row has two columns, rest have one column, with column label as first column in table:')
+      html_dsl.line_break()
+      html = create_table_view(data, 'Demo', ['field1', 'field2', 'field3', 'field4', 'field5'], [2, 1, 1, 1], {header_as_first_column:true, no_header:true, show_checkbox_for_row:true, auto_indent:true})
+      html_dsl.inject(html)
+    end
+
+    html_dsl.line_break()
+
+    # Nested tables:
+    html_dsl.div() do
+      html_dsl.label('Nested table, one field per row, with inner rows of two columns:')
+      html_dsl.line_break()
+      options = {header_as_first_column:true, no_header:true, show_checkbox_for_row:true, auto_indent:true}
+      html = create_table_view(data,
+                               'Demo',
+                               ['field1'],
+                               [1],
+                               options) do  |record, node|
+        gen = HtmlGenerator.new() {@xdoc = node.xml_document; @current_node = node}
+        gen.div(nil, nil, 'margin-left:10px')
+        # Child data can of course be dependent on parent record passed into block.
+        child_html = create_table_view(@child_data, 'Child', ['field2', 'field3'], [2], {no_header: true})  # can be recursive, etc...
+        gen.inject(child_html)
+        gen.div_end()
+      end
+
+      html_dsl.inject(html)
+    end
+
+    @html = get_html(html_dsl.html_gen.xdoc).html_safe
   end
 
   def my_pages
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,9 @@ require 'clifton_lib/xml/xml_document'
 require 'clifton_lib/xml/xml_text_writer'
 include CliftonXml
 
+# TODO: :one_column_per_row is obsolete
+# TODO: :show_checkboxes is obsolete
+
 module ApplicationHelper
   def show_field_error(model, field)
     s=''
@@ -14,52 +17,61 @@ module ApplicationHelper
   end
 
   # Return HTML to represent the following record collection as a table.
-  def create_table_view(table, table_name, columns, options = {}, &block)
+  # Parameters:
+  # table: The instance (usually a model) containing the table data
+  # table_name: The name of the table, used to create the qualified row id's for the optional associated checkboxes:
+  #    #{table_name}[record_#{row[:id]}]
+  # columns: Array of column names to display
+  # format: Array describing how a record's fields map to columns and rows.
+  #    For example, a 5 field table displaying one record per line would have a format:
+  #        [5]
+  #    A table with 3 fields on one row and 2 fields on the second row:
+  #        [3, 2]
+  #    A table with one field per row, where there are 5 fields
+  #        [1, 1, 1, 1, 1]
+  # options: Various options that can be specified
+  #    :id - the table element ID
+  #    :class - the table classes, as a single string, for example: "class1 class2 class3"
+  #    :no_header - do not create a table header section
+  #    :show_checkbox_for_row - put a checkbox as the first column of the first row
+  #    :auto_indent - for subsequent rows, auto-indent.  Applies only when :show_checkbox_for_row is true.
+  #    :header_as_first_column - headers for all columns on the row go into the first column
+  # &block: yield to caller at each row for creating sub-tables
+  def create_table_view(table, table_name, columns, format, options = {}, &block)
     xdoc = XmlDocument.new()
     table_node = xdoc.create_element('table')
-
-    # append a class name for specific styling.
-    if options[:class]
-      table_node.append_attribute(xdoc.create_attribute('class', options[:class]))
-    end
-
+    append_attribute(table_node, options, :id)
+    append_attribute(table_node, options, :class)
     xdoc.append_child(table_node)
-
-    # We may not want to display the header, for example, nested tables in a "tree view" kind of display.
-    unless options[:no_header]
-      create_table_header(xdoc, table_node, columns, options)
-    end
-
-    create_table_content(xdoc, table_node, table, table_name, columns, options, &block)
-
+    create_table_header(table_node, columns, format, options)
+    create_table_content(table_node, table, table_name, columns, format, options, &block)
     get_html(xdoc).html_safe
   end
 
   # create just the headers for the table inside a <thead> section.
-  def create_table_header(xdoc, table_node, columns, options = {})
-    table_head_node = xdoc.create_element('thead')
-    table_node.append_child(table_head_node)
+  def create_table_header(table_node, columns, format, options = {})
+    unless options[:no_header]
+      table_head_node = create_element(table_node, 'thead')
+      col_idx = 0
 
-    table_row_node = xdoc.create_element('tr')
-    table_head_node.append_child(table_row_node)
+      format.each_with_index do |cols_per_row, idx|
+        table_row_node = create_element(table_head_node, 'tr')
 
-    if options[:one_column_per_row]
-      # create two empty columns, in which we will put the data as "column name:" | "data"
-      table_heading_node = xdoc.create_element('th')
-      table_row_node.append_child(table_heading_node)
-    else
-      # create an empty column if showing a checkbox for the row.
-      if options[:show_checkbox_for_row]
-        table_heading_node = xdoc.create_element('th')
-        table_row_node.append_child(table_heading_node)
-      end
+        # If first index and we want to show checkboxes, then create a placeholder header column
+        if (idx == 0 || options[:auto_indent]) && options[:show_checkbox_for_row]
+          create_element(table_row_node, 'th')
+        end
 
-      columns.each do |column|
-        table_heading_node = xdoc.create_element('th')
-        table_heading_node.inner_text = column.gsub('_', ' ').titleize
-        # titleize is a Rails function.  For straight Ruby:
-        # "kirk douglas".split(" ").map(&:capitalize).join(" ")
-        table_row_node.append_child(table_heading_node)
+        # For the # of columns that we are going to display in this row:
+        (1..cols_per_row).each do
+          # Create the header
+          column = columns[col_idx]
+          table_heading_node = create_element(table_row_node, 'th')
+          table_heading_node.inner_text = column.gsub('_', ' ').titleize
+          # titleize is a Rails function.  For straight Ruby:
+          # "kirk douglas".split(" ").map(&:capitalize).join(" ")
+          col_idx += 1
+        end
       end
     end
 
@@ -67,17 +79,60 @@ module ApplicationHelper
   end
 
   # Create the table body content.  No attention is paid to paging, etc.
-  def create_table_content(xdoc, table_node, table, table_name, columns, options)
-    table_body_node = xdoc.create_element("tbody")
-    table_node.append_child(table_body_node)
+  def create_table_content(table_node, table, table_name, columns, format, options)
+    table_body_node = create_element(table_node, "tbody")
 
-    table.each do |row|
+    table.each_with_index do |row, row_idx|
+      col_idx = 0
+
+      format.each_with_index do |cols_per_row, idx|
+        table_row_node = create_element(table_body_node, 'tr')
+
+        if idx == 0 && options[:show_checkbox_for_row]
+          table_data_node = create_element(table_row_node, 'td')
+          input_node = create_element(table_data_node, 'input')
+          create_attribute(input_node, 'type', 'checkbox')
+          create_attribute(input_node, 'name', record_name(table_name, row, row_idx))
+        elsif idx > 0 && options[:show_checkbox_for_row] && options[:auto_indent]
+          # create an empty column for subsequent rows to indent the data.
+          create_element(table_row_node, 'td')
+        end
+
+        if options[:header_as_first_column]
+          hdr_col = create_element(table_row_node, 'td')
+          # The first column joins all the header names into a comma-separated list for the columns that go on this row.
+          hdr_col.inner_text = columns[col_idx..col_idx+cols_per_row-1].join(', ')+':'
+        end
+
+        (1..cols_per_row).each do
+          column = columns[col_idx]
+          col_idx += 1
+          table_data_node = create_element(table_row_node, 'td')
+          text = row[column].to_s
+
+          # Replace text with a custom specified renderer.
+          if options[:custom_text_renderers]
+            renderers = options[:custom_text_renderers]
+            if renderers[column.to_sym]
+              text = renderers[column.to_sym].call(text)
+            end
+          end
+
+          table_data_node.inner_text = text
+        end
+
+        # Allow caller to specify additional HTML in the <tr>, for example to allow nesting of tables.
+        if block_given?
+          yield(row, table_row_node)
+        end
+      end
+    end
+=begin
       if options[:one_column_per_row]
         # first row is just a checkbox.
         # TODO: Caller should be able to label this checkbox perhaps.
         if options[:show_checkbox_for_row]
-          table_row_node = xdoc.create_element('tr')
-          table_body_node.append_child(table_row_node)
+          table_row_node = create_element(table_body_node, 'tr')
 
           # first row, add a class allowing us to delineate records by some styling
           table_row_node.append_attribute(xdoc.create_attribute('class', 'next-record'))
@@ -148,7 +203,7 @@ module ApplicationHelper
             end
 
             div.inner_text = text
-
+=end
 =begin
             # the second column is the data
             table_data_node = xdoc.create_element('td')
@@ -167,6 +222,7 @@ module ApplicationHelper
             table_data_node.inner_text = text
             table_row_node.append_child(table_data_node)
 =end
+=begin
           end
         end
       else
@@ -223,7 +279,7 @@ module ApplicationHelper
         end
       end
     end
-
+=end
     nil
   end
 
@@ -239,7 +295,7 @@ module ApplicationHelper
 
   # TODO: Replace with a real tree-view, rather than one generated from nested tables.
   def create_category_table(categories, options={})
-    category_html = create_table_view(categories, 'category_list', ["name"], {show_checkboxes: true}.merge(options)) { |category, node|
+    category_html = create_table_view(categories, 'category_list', ["name"], [1], {show_checkbox_for_row: true}.merge(options)) { |category, node|
       # Process child categories of the parent that has been passed in.
       child_categories = Category.where("category_id = #{category.id}").order('name ASC')
 
@@ -327,4 +383,43 @@ module ApplicationHelper
   javascript = "<script type='text/javascript'>\r\n//<![CDATA[\r\n"
   javascript << "//]]>\r\n</script>\r\n"
   end
+
+  # TODO: Consider using the function in the html_generator and html_dsl to create optional attributes.
+  # Append an attribute of the specified symbol if it exists in the options list, with an optional
+  # attribute name override for the symbol.
+  def append_attribute(node, options, attr_sym, override_attr_name = nil)
+    # Use the provided override attribute name if it exists, otherwise use the symbol
+    attr_name = override_attr_name ? override_attr_name : attr_sym.to_s
+
+    if options[attr_sym]
+      node.append_attribute(node.xml_document.create_attribute(attr_name, options[attr_sym]))
+    end
+  end
+
+  # If the record has an ID field, then we use that for the name checkbox record field,
+  # otherwise we use the row count index.
+  def record_name(table_name, row, row_idx)
+    if row.respond_to?(:id)
+      "#{table_name}[record_#{row[:id]}]"
+    else
+      "#{table_name}[record_#{row_idx}]"
+    end
+  end
+
+  # TODO: Move these into clifton_lib as helper functions?
+
+  # Create a child element of the given name for the specified node.
+  # Return the child.
+  def create_element(node, element_name)
+    child = node.xml_document.create_element(element_name)
+    node.append_child(child)
+
+    child
+  end
+
+  # Create an attribute on the given node with the specified name and value.
+  def create_attribute(node, attr_name, attr_val)
+    node.append_attribute(node.xml_document.create_attribute(attr_name, attr_val))
+  end
+
 end
